023c8ce606b1106df2a8dc29e39131925865d327
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-05-18 16:55:38 +0800

    feat: update about page and readme file

--- src/pages/AboutScreen.tsx
+++ src/pages/AboutScreen.tsx
@@ -16,32 +16,45 @@ const AboutScreen: React.FC<NavigationProps> = ({ navigation }) => {
         navigation={navigation}
       />
       <ScrollView style={tw`p-5`}>
-        <View style={tw`mb-10`}>
+        <View style={tw`mb-5`}>
           <View style={tw`flex flex-col items-start mb-3`}>
             <Text style={[tw`text-2xl text-[#FB8500] uppercase`, fonts.fontPoppinsBlack]}>System</Text>
             <Text style={[tw`text-[16px] text-[#023047]`, fonts.fontPoppinsSemiBold]}>
-              Subtitle
+              Apps & Data
             </Text>
           </View>
           <View style={tw`flex flex-col items-start`}>
             <Text style={[tw`text-base text-[#333333] mb-2`, fonts.fontPoppins]}>
-              Text Here
+              DreamDiary® is a personal diary of your dream, so you can remember all your dreams, and to help your mental health.
+              Your dreams are captured your life.
             </Text>
-            <Text style={[tw`text-xs text-zinc-500`, fonts.fontPoppinsLight]}>{new Date().getFullYear()}</Text>
+            <Text style={[tw`text-base text-[#333333] mb-2`, fonts.fontPoppins]}>
+              Write your dreams in one place, so you won't forget.
+            </Text>
+            <Text style={[tw`text-base text-[#333333] mb-2`, fonts.fontPoppins]}>
+              Your data will stored to your local devices. Be careful for deleting this app data.
+              It will cause permanent loss of your diaries.
+            </Text>
           </View>
         </View>
         <View>
           <View style={tw`flex flex-col items-start mb-3`}>
             <Text style={[tw`text-2xl text-[#FB8500] uppercase`, fonts.fontPoppinsBlack]}>Developer</Text>
             <Text style={[tw`text-[16px] text-[#023047]`, fonts.fontPoppinsSemiBold]}>
-              Subtitle
+              Designed & Developed
             </Text>
           </View>
           <View style={tw`flex flex-col items-start`}>
             <Text style={[tw`text-base text-[#333333] mb-2`, fonts.fontPoppins]}>
-              Text Here
+              This app is dedicated to the person who has depressions or anxiety.
+              I will continue to add some features to this app for the better user experience.
             </Text>
-            <Text style={[tw`text-xs text-zinc-500`, fonts.fontPoppinsLight]}>{new Date().getFullYear()}</Text>
+            <Text style={[tw`text-base text-[#333333] mb-2`, fonts.fontPoppins]}>
+              Proudly created with ❤️ by VEOSCRIPT.
+            </Text>
+            <Text style={[tw`text-xs text-zinc-500 mt-3`, fonts.fontPoppinsLight]}>
+              DreamDiary®. All rights reserved {new Date().getFullYear()}.
+            </Text>
           </View>
         </View>
       </ScrollView>
